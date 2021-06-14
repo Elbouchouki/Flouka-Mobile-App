1c90d7f0d5b28059104371ad5ab6dbbe3814a1f6
Elbouchouki <elbouchoukigamer@gmail.com>
2021-06-14 12:01:05 +0100

    api sousCategories

--- lib/controllers/cartController.dart
+++ lib/controllers/cartController.dart
@@ -1,50 +1,53 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_auth/models/cart.dart';
 import 'package:flutter_auth/models/produit.dart';
-import 'package:flutter_auth/models/stock.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
-import 'dart:convert';
 
 class CartController extends GetxController {
   final _session = GetStorage();
 
   // ignore: deprecated_member_use
-  var _cartList = List<Stock>().obs;
+  var _cartList = List<Cart>().obs;
   var total = 0.0.obs;
-  List<Stock> get cartList => _cartList.value;
-  void calculateTotal() {}
+  List<Cart> get cartList => _cartList.value;
+  void calculateTotal() {
+    double t = 0;
+    _cartList.value.forEach((cart) {
+      t += cart.totalPrice;
+    });
+    total.value = t;
+  }
 
   @override
   void onInit() {
-    // _cartList.value =
-    //     Produit.fromJson(_session.read("cartList").toString());
-    // calculateTotal();
+    _cartList.value = Cart.cartFromJson(_session.read("cartList").toString());
+    calculateTotal();
     super.onInit();
   }
 
-  void addProduct(Produit product) {
-    // _cartList.add(product);
-    // calculateTotal();
+  void addProduct(Cart cart) {
+    _cartList.add(cart);
+    calculateTotal();
 
-    // _sessionSave();
+    _sessionSave();
   }
 
-  void deleteProduct(Produit product) {
-    // _cartList.remove(product);
-    // calculateTotal();
+  // void deleteProduct(Cart cart) {
+  //   _cartList.remove(cart);
+  //   calculateTotal();
 
-    // _sessionSave();
-  }
+  //   _sessionSave();
+  // }
 
   void deleteProductByIndex(int index) {
-    // _cartList.removeAt(index);
-    // calculateTotal();
+    _cartList.removeAt(index);
+    calculateTotal();
 
-    // // _cartList.value.removeAt(index);
-    // _sessionSave();
+    // _cartList.value.removeAt(index);
+    _sessionSave();
   }
 
   void _sessionSave() {
-    // _session.write("cartList", Produit.productToJson(_cartList.value));
+    _session.write("cartList", Cart.cartToJson(_cartList.value));
   }
 }
