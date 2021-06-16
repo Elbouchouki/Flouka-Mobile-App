a1050303492bc21ff122e9c5a2a8235d23a581d9
Elbouchouki <elbouchoukigamer@gmail.com>
2021-06-16 12:36:00 +0100

    calcule price changing

--- lib/controllers/detailsController.dart
+++ lib/controllers/detailsController.dart
@@ -1,10 +1,13 @@
 import 'package:collection/collection.dart';
+import 'package:flutter_auth/controllers/cartController.dart';
 import 'package:flutter_auth/controllers/stockController.dart';
+import 'package:flutter_auth/models/cart.dart';
 import 'package:flutter_auth/models/stock.dart';
 import 'package:get/get.dart';
 
 class DetailsController extends GetxController {
   StockController stockController = Get.find();
+  CartController cartController = Get.find();
   var listProducts = List<List<Stock>>().obs;
   var qte = List<List<int>>().obs;
   var total = 0.0.obs;
@@ -16,10 +19,38 @@ class DetailsController extends GetxController {
             .where((element) => element.produitId == p.produitId)
             .toList(),
         (s) => s.trancheId).forEach((key, value) {
-      listProducts.value.add(value);
-      qte.value.add([0, sum(value)]);
+      if (cartController.cartList.length != 0) {
+        cartController.cartList.forEach((cart) {
+          if (value[0].categorieId.toString() +
+                  value[0].produitId.toString() +
+                  value[0].trancheId ==
+              cart.produits[0].categorieId.toString() +
+                  cart.produits[0].produitId.toString() +
+                  cart.produits[0].trancheId) {
+            listProducts.value.add(value);
+            /* ----------------------------------- qsd ---------------------------------- */
+            qte.value.add([0, cart.qte]);
+          }
+        });
+      } else {
+        listProducts.value.add(value);
+        qte.value.add([0, sum(value)]);
+      }
     });
-    print(qte.value);
+  }
+
+  addToCart() {
+    for (var i = 0; i < listProducts.length; i++) {
+      if (qte.value[i][0] > 0) {
+        cartController.addProduct(Cart(
+            id: listProducts.value[i][0].categorieId.toString() +
+                listProducts.value[i][0].produitId.toString() +
+                listProducts.value[i][0].lotNum,
+            produits: listProducts.value[i],
+            qte: qte.value[i][0],
+            totalPrice: total.value));
+      }
+    }
   }
 
   int sum(List<Stock> produits) {
@@ -38,6 +69,7 @@ class DetailsController extends GetxController {
       return;
     }
     qte.value[index][0] += pas;
+
     _commitChange();
   }
 
@@ -62,6 +94,16 @@ class DetailsController extends GetxController {
   calculeTotal() {
     var t = 0.0;
     for (var i = 0; i < listProducts.length; i++) {
+      if (double.parse(listProducts.value[i][0].poids) != 0) {
+        for (var j = 0; j < qte[i][0]; j++) {
+          // print(double.parse(listProducts.value[i][j].poids));
+          // print(double.parse(listProducts.value[i][j].poids) *
+          //     double.parse(listProducts.value[i][j].prixN));
+          t += double.parse(listProducts.value[i][j].poids) *
+              double.parse(listProducts.value[i][j].prixN);
+        }
+        continue;
+      }
       t += double.parse(listProducts.value[i][0].prixN) * qte.value[i][0];
     }
     total.value = t;
@@ -70,12 +112,6 @@ class DetailsController extends GetxController {
   _commitChange() {
     qte.refresh();
     calculeTotal();
-  }
-
-  addToCart() {
-    for (var i = 0; i < listProducts.length; i++) {
-      listProducts.value[i];
-    }
   }
 
   @override
